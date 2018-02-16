f344daefab07b76952d17ac5acfd050f73775d1e
adannup <zad3life@gmail.com>
2018-02-15 23:39:52 -0600

    Add mongooseDB functions

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,50 +1,100 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET request to /products',
-  });
+  Product.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      if(docs.length > 0) {
+        res.status(200).json(docs);
+      } else {
+        res.status(404).json({
+          message: 'No entries found',
+        })
+      }
+
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      })
+    })
 });
 
 router.post('/', (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
-  };
-
-  res.status(201).json({
-    message: 'Handling POST request to /products',
-    createdProduct: product,
   });
+  product.save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Handling POST request to /products',
+        createdProduct: result,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      })
+    });
 });
 
 router.route('/:productId')
   .get((req, res) => {
     const id = req.params.productId;
-    if(id === 'special') {
-      res.status(200).json({
-        message: 'You discover the special ID',
-        id,
+    Product.findById(id)
+      .exec()
+      .then(doc => {
+        console.log('From database:', doc);
+        if(doc) {
+          res.status(200).json(doc);
+        } else {
+          res.status(404).json({ message: 'No valid entry found for provided ID'});
+        }
       })
-    }
-    res.status(200).json({
-      message: 'Response ID',
-      id,
-    });
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+      });
   })
   .patch((req, res) => {
     const id = req.params.productId;
-    res.status(200).json({
-      message: 'Updated product!',
-      id,
-    });
+    const updateOps = {};
+    for(let ops of req.body) {
+      updateOps[ops.propName] = ops.value;
+    }
+    Product.update({ _id: id }, { $set: updateOps })
+      .exec()
+      .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+      });
+    // Product.update({ _id: id }, { $set: { name: req.body.name, req.body.price }})
   })
   .delete((req, res) => {
     const id = req.params.productId;
-    res.status(200).json({
-      message: 'Delete product',
-    });
+    Product.remove({_id: id})
+      .exec()
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err,
+        })
+      })
   })
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
+mongoose.connect(`mongodb://adannup:${process.env.MONGO_ATLAS_PWD}@node-rest-shop-shard-00-00-fvtie.mongodb.net:27017,node-rest-shop-shard-00-01-fvtie.mongodb.net:27017,node-rest-shop-shard-00-02-fvtie.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin`);
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// CORS CONFIG
 app.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -37,6 +41,7 @@ app.use((req, res, next) => {
   next(err);
 });
 
+// Error handlers
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
