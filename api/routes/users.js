1bf2436561a56ed9baddc52d56d7252f32710dcd
adannup <zad3life@gmail.com>
2018-02-20 22:11:49 -0600

    add user login & JWT signing

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const router = express.Router();
@@ -41,6 +42,41 @@ router.post('/signup', (req, res) => {
         })
       }
     });
+});
+
+router.post('/login', (req, res) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if(user.length < 1) {
+        return res.status(401).json({
+          message: 'Auth failed',
+        });
+      } else {
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+          if (err || !result) {
+            return res.status(401).json({
+              message: 'Auth failed',
+            });
+          } else {
+            const token = jwt.sign({
+              email: user[0].email,
+              userId: user[0]._id,
+            }, process.env.JWT_KEY, {
+              expiresIn: '1h',
+            });
+
+            return res.status(200).json({
+              message: 'Auth successful',
+              token,
+            });
+          }
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    })
 });
 
 router.delete('/:userId', (req, res) => {
